139181809590414760de41c0dee1fb78793a8275
Paul Annesley <paul@annesley.cc>
2011-10-03 21:41:48 +1100

    Abbreviated registers to reg in instruction_integration_spec.

--- spec/instruction_integration_spec.rb
+++ spec/instruction_integration_spec.rb
@@ -5,15 +5,15 @@ module C64
   describe Cpu do
 
     def cpu; @cpu ||= C64::Cpu.new; end
-    def registers; cpu.send :registers; end
-    def status; registers.status; end
+    def reg; cpu.send :registers; end
+    def status; reg.status; end
     def memory; cpu.send :memory; end
 
     # Usage: run_instructions "AA BB", "CC DD EE", at: 0x400
     def run_instructions *i
       options = if i.last.is_a? Hash then i.pop else {} end
       offset = options[:at] || 0
-      registers.pc = offset
+      reg.pc = offset
       i.each do |instruction|
         instruction.split(" ").each do |hex|
           memory[offset] = hex.to_i(16)
@@ -25,62 +25,62 @@ module C64
 
     describe :AND do
       it "bitwise AND into accumulator" do
-        registers.ac = 0b01010101
+        reg.ac = 0b01010101
         run_instructions "29 F0" # AND #F0
-        registers.ac.must_equal 0b01010000
+        reg.ac.must_equal 0b01010000
       end
     end
 
     describe :BEQ do
       it "branches forwards for zero? true" do
-        registers.sr = 0b00000010
+        reg.sr = 0b00000010
         run_instructions "F0 04", at: 0x0400
-        registers.pc.must_equal 0x0406
+        reg.pc.must_equal 0x0406
       end
       it "branches backwards for zero? true" do
-        registers.sr = 0b00000010
+        reg.sr = 0b00000010
         run_instructions "F0 F8", at: 0x0406
-        registers.pc.must_equal 0x0400
+        reg.pc.must_equal 0x0400
       end
       it "does not branch for zero? false" do
-        registers.sr = 0b00000000
+        reg.sr = 0b00000000
         run_instructions "F0 08", at: 0x0400
-        registers.pc.must_equal 0x0402
+        reg.pc.must_equal 0x0402
       end
     end
 
     describe :BNE do
       it "branches forwards for zero? false" do
-        registers.sr = 0b00000000
+        reg.sr = 0b00000000
         run_instructions "D0 04", at: 0x0400
-        registers.pc.must_equal 0x0406
+        reg.pc.must_equal 0x0406
       end
       it "branches backwards for zero? false" do
-        registers.sr = 0b00000000
+        reg.sr = 0b00000000
         run_instructions "D0 F8", at: 0x0406
-        registers.pc.must_equal 0x0400
+        reg.pc.must_equal 0x0400
       end
       it "does not branch for zero? true" do
-        registers.sr = 0b00000010
+        reg.sr = 0b00000010
         run_instructions "D0 08", at: 0x0400
-        registers.pc.must_equal 0x0402
+        reg.pc.must_equal 0x0402
       end
     end
 
     describe :CLD do
       it "clears decimal mode" do
-        registers.status.decimal = true
+        reg.status.decimal = true
         run_instructions "D8"
-        registers.status.decimal?.must_equal false
+        reg.status.decimal?.must_equal false
       end
     end
 
     describe :CMP do
       it "compares memory with accumulator" do
-        registers.sr = 0
+        reg.sr = 0
         memory[0xDEAD] = 0x01
         run_instructions "A9 01", "DD AD DE" # LDA #01, CMP 0x0000,x
-        registers.status.tap do |s|
+        reg.status.tap do |s|
           s.zero?.must_equal true
           s.carry?.must_equal true
           s.negative?.must_equal false
@@ -99,14 +99,14 @@ module C64
     describe :DEX do
       it "decrements x by one" do
         run_instructions "A2 AA", "CA" # LDX #AA, DEX
-        registers.x.must_equal 0xA9
+        reg.x.must_equal 0xA9
       end
     end
 
     describe :DEY do
       it "decrements y by one" do
         run_instructions "A0 AA", "88" # LDY #AA, DEY
-        registers.y.must_equal 0xA9
+        reg.y.must_equal 0xA9
       end
     end
 
@@ -121,28 +121,28 @@ module C64
     describe :INX do
       it "increments x by one" do
         run_instructions "E8"
-        registers.x.must_equal 1
+        reg.x.must_equal 1
       end
     end
 
     describe :INY do
       it "increments y by one" do
         run_instructions "C8"
-        registers.y.must_equal 1
+        reg.y.must_equal 1
       end
     end
 
     describe :JMP do
       it "updates program counter" do
         run_instructions "4C AD DE" # JMP #0xDEAD
-        registers.pc.must_equal 0xDEAD
+        reg.pc.must_equal 0xDEAD
       end
     end
 
     describe :JSR do
       it "stores PC, jumps to address" do
         run_instructions "20 AD DE", at: 1000
-        registers.pc.must_equal 0xDEAD
+        reg.pc.must_equal 0xDEAD
 
         # The return address pushed to the stack by JSR is that of the
         # last byte of the JSR operand (that is, the most significant
@@ -151,113 +151,113 @@ module C64
         # http://en.wikipedia.org/wiki/MOS_Technology_6502#Bugs_and_quirks
 
         # little-endian 0xEA03 == 1002
-        memory[registers.sp + 1].must_equal 0xEA
-        memory[registers.sp + 2].must_equal 0x03
+        memory[reg.sp + 1].must_equal 0xEA
+        memory[reg.sp + 2].must_equal 0x03
       end
     end
 
-    { ac: 0xA9, x: 0xA2, y: 0xA0 }.each do |reg, op|
-      describe "LD#{reg.to_s[0].upcase} immediate" do
-        it "loads immediate value into #{reg} register" do
+    { ac: 0xA9, x: 0xA2, y: 0xA0 }.each do |r, op|
+      describe "LD#{r.to_s[0].upcase} immediate" do
+        it "loads immediate value into #{r} register" do
           run_instructions "#{op.to_s(16)} AA"
-          registers[reg].must_equal 0xAA
+          reg[r].must_equal 0xAA
         end
       end
     end
 
-    { ac: 0xA5, x: 0xA6, y: 0xA4 }.each do |reg, op|
-      describe "LD#{reg.to_s[0].upcase} zeropage" do
-        it "loads zeropage value into #{reg} register" do
+    { ac: 0xA5, x: 0xA6, y: 0xA4 }.each do |r, op|
+      describe "LD#{r.to_s[0].upcase} zeropage" do
+        it "loads zeropage value into #{r} register" do
           memory[0x10] = 0xAA
           run_instructions "#{op.to_s(16)} 10"
-          registers[reg].must_equal 0xAA
+          reg[r].must_equal 0xAA
         end
       end
     end
 
-    { ac: 0xB5, y: 0xB4 }.each do |reg, op|
-      describe "LD#{reg.to_s[0].upcase} zeropage_x" do
-        it "loads zeropage X-indexed value into #{reg} register" do
-          registers.x = 0x04
+    { ac: 0xB5, y: 0xB4 }.each do |r, op|
+      describe "LD#{r.to_s[0].upcase} zeropage_x" do
+        it "loads zeropage X-indexed value into #{r} register" do
+          reg.x = 0x04
           memory[0x10 + 0x04] = 0xAA
           run_instructions "#{op.to_s(16)} 10"
-          registers[reg].must_equal 0xAA
+          reg[r].must_equal 0xAA
         end
       end
     end
 
     describe "LDX zeropage_y" do
       it "loads zeropage Y-indexed value into X register" do
-        registers.y = 0x04
+        reg.y = 0x04
         memory[0x10 + 0x04] = 0xAA
         run_instructions "B6 10"
-        registers.x.must_equal 0xAA
+        reg.x.must_equal 0xAA
       end
     end
 
     describe "LDA setting SR flags" do
       it "sets zero flag off" do
-        registers.status.zero = true
+        reg.status.zero = true
         run_instructions "A9 01"
-        registers.status.zero?.must_equal false
+        reg.status.zero?.must_equal false
       end
       it "sets zero flag on" do
         run_instructions "A9 00"
-        registers.status.zero?.must_equal true
+        reg.status.zero?.must_equal true
       end
     end
 
     describe :NOP do
       it "does nothing" do
         run_instructions "EA"
-        registers.pc.must_equal 1
+        reg.pc.must_equal 1
       end
     end
 
     describe :ORA do
       it "bitwise OR into accumulator" do
-        registers.ac = 0b01010101
+        reg.ac = 0b01010101
         run_instructions "09 F0" # AND #F0
-        registers.ac.must_equal 0b11110101
+        reg.ac.must_equal 0b11110101
       end
     end
 
     describe :ROL do
       it "shifts left one bit, carry into low bit, high bit into carry" do
-        registers.status.carry = false
-        registers.ac = 0b10101010
+        reg.status.carry = false
+        reg.ac = 0b10101010
         run_instructions "2A"
-        registers.ac.must_equal 0b01010100
-        registers.status.carry?.must_equal true
+        reg.ac.must_equal 0b01010100
+        reg.status.carry?.must_equal true
       end
     end
 
     describe :ROR do
       it "shifts right one bit, carry into high bit, low bit into carry" do
-        registers.status.carry = true
-        registers.ac = 0b10101010
+        reg.status.carry = true
+        reg.ac = 0b10101010
         run_instructions "6A"
-        registers.ac.must_equal 0b11010101
-        registers.status.carry?.must_equal false
+        reg.ac.must_equal 0b11010101
+        reg.status.carry?.must_equal false
       end
     end
 
     describe :RTS do
       it "returns from subroutine, restores stack pointer" do
-        sp = registers.sp
+        sp = reg.sp
         memory[0xDEAD] = 0x60       # RTS
         run_instructions "20 AD DE" # JSR to 0xDEAD
         cpu.step
-        registers.pc.must_equal 0x03
-        registers.sp.must_equal sp
+        reg.pc.must_equal 0x03
+        reg.sp.must_equal sp
       end
     end
 
     describe :SEI do
       it "sets interrupt disable" do
-        registers.status.interrupt = false
+        reg.status.interrupt = false
         run_instructions "78"
-        registers.status.interrupt?.must_equal true
+        reg.status.interrupt?.must_equal true
       end
     end
 
@@ -278,27 +278,27 @@ module C64
     describe :TAX do
       it "transfers accumulator to X" do
         run_instructions "A9 AA", "AA" # LDA, TAX
-        registers.x.must_equal 0xAA
+        reg.x.must_equal 0xAA
       end
     end
 
     describe :TAY do
       it "transfers accumulator to Y" do
         run_instructions "A9 AA", "A8" # LDA, TAY
-        registers.y.must_equal 0xAA
+        reg.y.must_equal 0xAA
       end
     end
 
     describe :TXA do
       it "transfers X to accumulator" do
         run_instructions "A2 AA", "A0 00", "8A" # LDX 0xAA, LDY 0x00, TXA
-        registers.ac.must_equal 0xAA
+        reg.ac.must_equal 0xAA
         status.zero?.must_equal false
         status.negative?.must_equal true
       end
       it "sets zero flag for zero value" do
         run_instructions "A2 00", "A0 01", "8A" # LDX 0x00, LDY 0x01, TXA
-        registers.ac.must_equal 0x00
+        reg.ac.must_equal 0x00
         status.zero?.must_equal true
         status.negative?.must_equal false
       end
@@ -307,13 +307,13 @@ module C64
     describe :TYA do
       it "transfers Y to accumulator" do
         run_instructions "A0 AA", "A2 00", "98" # LDY 0xAA, LDX 0x00, TYA
-        registers.ac.must_equal 0xAA
+        reg.ac.must_equal 0xAA
         status.zero?.must_equal false
         status.negative?.must_equal true
       end
       it "sets zero flag for zero value" do
         run_instructions "A0 00", "A2 01", "98" # LDY 0x00, LDX 0x01, TYA
-        registers.ac.must_equal 0x00
+        reg.ac.must_equal 0x00
         status.zero?.must_equal true
         status.negative?.must_equal false
       end
@@ -322,7 +322,7 @@ module C64
     describe :TXS do
       it "transfers X to stack pointer" do
         run_instructions "A2 AA", "9A" # LDS, TXS
-        registers.sp.must_equal 0xAA
+        reg.sp.must_equal 0xAA
       end
     end
 
