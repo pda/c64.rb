d5f4ce96839b35a4c741ed5d07c741ae9f438f9a
Paul Annesley <paul@annesley.cc>
2011-10-11 13:45:00 +1100

    Test script loads ROMs into AddressBus.

--- test.rb
+++ test.rb
@@ -2,28 +2,22 @@
 $LOAD_PATH.unshift "."
 require "c64/cpu"
 require "c64/memory"
-require "c64/memory_overlay"
+require "c64/address_bus"
 
 module C64
 
-  ram =    Memory.new(0x10000) # 64k
-  kernal = Memory.new(0x2000, image: "rom/kernal.rom") # 8k
-  char =   Memory.new(0x1000, image: "rom/character.rom") # 4k
-  basic =  Memory.new(0x2000, image: "rom/basic.rom") # 8k
+  # TODO: why is BASIC not being read?
+  # TODO: check implied (+indexed) addressing.. carry?
 
-  memory = MemoryOverlay.new(
-    MemoryOverlay.new(
-      MemoryOverlay.new(
-        ram,
-        kernal,
-        0xE000
-      ),
-      char,
-      0xD000
-    ),
-    basic,
-    0xA000
-  )
+  memory = AddressBus.new \
+    ram: Memory.new(0x10000, tag: nil), # 64k
+    kernal: Memory.new(0x2000, image: "rom/kernal.rom"), # 8k
+    basic: Memory.new(0x2000, image: "rom/basic.rom", tag: "BASIC"), # 8k
+    char: Memory.new(0x1000, image: "rom/character.rom", tag: "CHAR"), # 4k
+    io: Memory.new(0x1000, tag: nil) # 4k
+
+  memory[0x0000] = 0b101111 # directions, 0:in, 1:out
+  memory[0x0001] = 0b000111 # data
 
   cpu = Cpu.new memory: memory
 
